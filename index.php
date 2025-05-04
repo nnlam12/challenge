3ed67ee3299eabd26f152cc1b6c027ffffb55439
nnlam12 <nhatlamnguyen121212@gmail.com>
2025-05-04 19:14:28 +0200

    Add error handling for headers already sent in index.php

--- index.php
+++ index.php
@@ -30,7 +30,11 @@
     <?php
     ob_start();
     error_reporting(E_ALL);
-ini_set('display_errors', 1);
+    ini_set('display_errors', 1);
+    if (headers_sent($file, $line)) {
+        die("Headers already sent in $file on line $line");
+    }
+
     session_start();
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $_SESSION['cat'] = true;
