bd6afbd5fb38e0759d5e8a05eea196c485e7c24d
nnlam12 <nhatlamnguyen121212@gmail.com>
2025-05-04 19:10:27 +0200

    Enable error reporting for debugging in index.php

--- index.php
+++ index.php
@@ -29,6 +29,8 @@
     </form>
     <?php
     ob_start();
+    error_reporting(E_ALL);
+ini_set('display_errors', 1);
     session_start();
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $_SESSION['cat'] = true;
